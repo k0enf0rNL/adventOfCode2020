d08f1d1589a56578b529565feedf21517d215556
Koen Konst <c.h.konst@avisi.nl>
2024-12-10 10:08:42 +0100

    2024 Day 10

--- src/main/kotlin/adventofcode/y2024/day10.kt
+++ src/main/kotlin/adventofcode/y2024/day10.kt
@@ -4,7 +4,9 @@ import adventofcode.utils.Point
 import adventofcode.utils.PointWithInt
 import adventofcode.utils.findAllPathsByPredicate
 import adventofcode.utils.getNeighbours
+import adventofcode.utils.println
 import adventofcode.utils.readInput
+import kotlin.time.measureTime
 
 fun main() {
     val input = readInput("2024/day10/input.txt").mapIndexed { rowIndex, row ->
@@ -19,16 +21,19 @@ fun main() {
         }
     }
     val startingPoints = input.flatMap { it.filter { it.value == 0 } }
-    val paths = startingPoints.map {
-        findAllPathsByPredicate(
-            it,
-            { it.value == 9 },
-            { pointWithInt -> input.getNeighbours(pointWithInt.point).filter { pointWithInt.value + 1 == it.value } }
-        )
-    }
-    val part1 = paths.map { it.mapNotNull { it.end }.toSet() }.sumOf { it.size }
-    println("Part 1: $part1")
 
-    val part2 = paths.map { it.mapNotNull { it.end } }.sumOf { it.size }
-    println("Part 2: $part2")
+    measureTime {
+        val paths = startingPoints.map {
+            findAllPathsByPredicate(
+                start = it,
+                endFunction = { it.value == 9 },
+                neighbours = { pointWithInt -> input.getNeighbours(pointWithInt.point).filter { pointWithInt.value + 1 == it.value } }
+            )
+        }
+        val part1 = paths.map { it.mapNotNull { it.end }.toSet() }.sumOf { it.size }
+        println("Part 1: $part1")
+
+        val part2 = paths.map { it.mapNotNull { it.end } }.sumOf { it.size }
+        println("Part 2: $part2")
+    }.println()
 }
