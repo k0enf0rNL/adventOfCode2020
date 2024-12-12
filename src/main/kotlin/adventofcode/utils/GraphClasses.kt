20fb7096e6836b62c3c3cf8695236a7dabf8af19
Koen Konst <c.h.konst@avisi.nl>
2024-12-12 16:10:32 +0100

    2024 day 12

---
+++ src/main/kotlin/adventofcode/utils/GraphClasses.kt
@@ -0,0 +1,113 @@
+package adventofcode.utils
+
+data class Point(val rowIndex: Long, val columnIndex: Long)
+data class PointInDirection(val point: Point, val direction: Direction)
+data class PointInDirectionWithLine(val point: Point, val direction: Direction, val line: Int)
+data class PointWithInt(val point: Point, val value: Int)
+data class PointWithChar(val point: Point, val value: Char)
+
+enum class Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+enum class DiagonalDirection {
+    UP_RIGHT,
+    UP_LEFT,
+    DOWN_RIGHT,
+    DOWN_LEFT
+}
+
+fun Point.getNextPoint(direction: Direction): Point =
+    when (direction) {
+        Direction.UP -> Point(rowIndex - 1, columnIndex)
+        Direction.DOWN -> Point(rowIndex + 1, columnIndex)
+        Direction.LEFT -> Point(rowIndex, columnIndex - 1)
+        Direction.RIGHT -> Point(rowIndex, columnIndex + 1)
+    }
+
+fun Point.getNextPointDiagonally(direction: DiagonalDirection): Point =
+    when (direction) {
+        DiagonalDirection.UP_RIGHT -> Point(rowIndex - 1, columnIndex + 1)
+        DiagonalDirection.DOWN_RIGHT -> Point(rowIndex + 1, columnIndex + 1)
+        DiagonalDirection.DOWN_LEFT -> Point(rowIndex + 1, columnIndex - 1)
+        DiagonalDirection.UP_LEFT -> Point(rowIndex - 1, columnIndex - 1)
+    }
+
+fun Direction.getOtherDirectionsWithoutGoingBack(): List<Direction> =
+    when (this) {
+        Direction.UP,
+        Direction.DOWN -> listOf(Direction.LEFT, Direction.RIGHT)
+
+        Direction.LEFT,
+        Direction.RIGHT -> listOf(Direction.UP, Direction.DOWN)
+    }
+
+fun Direction.rotateRight(): Direction =
+    when (this) {
+        Direction.UP -> Direction.RIGHT
+        Direction.DOWN -> Direction.LEFT
+        Direction.LEFT -> Direction.UP
+        Direction.RIGHT -> Direction.DOWN
+    }
+
+fun <T> List<List<T>>.getPointOrNull(point: Point): T? =
+    getOrNull(point.rowIndex.toInt())?.getOrNull(point.columnIndex.toInt())
+
+fun <T> List<List<T>>.getNeighbours(point: Point): List<T> = Direction.entries.toTypedArray().mapNotNull {
+    getPointOrNull(point.getNextPoint(it))
+}
+
+fun <T> List<List<T>>.getNeighboursDiagonally(point: Point): List<Pair<T?, DiagonalDirection>> = DiagonalDirection.entries.toTypedArray().map {
+    getPointOrNull(point.getNextPointDiagonally(it)) to it
+}
+
+fun <T> List<List<T>>.getNeighboursWithDirection(point: Point): List<Pair<T?, Direction>> = Direction.entries.toTypedArray().map {
+    getPointOrNull(point.getNextPoint(it)) to it
+}
+
+fun List<List<PointWithChar>>.countCorners(pointWithChar: PointWithChar): Long {
+    val diagonals = getNeighboursDiagonally(pointWithChar.point)
+    val neighbours = getNeighboursWithDirection(pointWithChar.point)
+    return diagonals.sumOf { diagonal ->
+        when (diagonal.second) {
+            DiagonalDirection.UP_RIGHT ->
+                if (diagonal.first?.value != pointWithChar.value && neighbours.first { it.second == Direction.UP }.first?.value == pointWithChar.value && neighbours.first { it.second == Direction.RIGHT }.first?.value == pointWithChar.value) {
+                    1L
+                } else if (neighbours.first { it.second == Direction.UP }.first?.value != pointWithChar.value && neighbours.first { it.second == Direction.RIGHT }.first?.value != pointWithChar.value) {
+                    1L
+                } else {
+                    0L
+                }
+
+            DiagonalDirection.UP_LEFT ->
+                if (diagonal.first?.value != pointWithChar.value && neighbours.first { it.second == Direction.UP }.first?.value == pointWithChar.value && neighbours.first { it.second == Direction.LEFT }.first?.value == pointWithChar.value) {
+                    1L
+                } else if (neighbours.first { it.second == Direction.UP }.first?.value != pointWithChar.value && neighbours.first { it.second == Direction.LEFT }.first?.value != pointWithChar.value) {
+                    1L
+                } else {
+                    0L
+                }
+
+            DiagonalDirection.DOWN_RIGHT ->
+                if (diagonal.first?.value != pointWithChar.value && neighbours.first { it.second == Direction.DOWN }.first?.value == pointWithChar.value && neighbours.first { it.second == Direction.RIGHT }.first?.value == pointWithChar.value) {
+                    1L
+                } else if (neighbours.first { it.second == Direction.DOWN }.first?.value != pointWithChar.value && neighbours.first { it.second == Direction.RIGHT }.first?.value != pointWithChar.value) {
+                    1L
+                } else {
+                    0L
+                }
+
+            DiagonalDirection.DOWN_LEFT ->
+                if (diagonal.first?.value != pointWithChar.value && neighbours.first { it.second == Direction.DOWN }.first?.value == pointWithChar.value && neighbours.first { it.second == Direction.LEFT }.first?.value == pointWithChar.value) {
+                    1L
+                } else if (neighbours.first { it.second == Direction.DOWN }.first?.value != pointWithChar.value && neighbours.first { it.second == Direction.LEFT }.first?.value != pointWithChar.value) {
+                    1L
+                } else {
+                    0L
+                }
+        }
+    }
+}
---
+++ src/main/kotlin/adventofcode/y2024/day12.kt
@@ -0,0 +1,70 @@
+package adventofcode.y2024
+
+import adventofcode.utils.Point
+import adventofcode.utils.PointWithChar
+import adventofcode.utils.countCorners
+import adventofcode.utils.getNeighbours
+import adventofcode.utils.getNeighboursWithDirection
+import adventofcode.utils.readInput
+
+fun main() {
+    val input = readInput("2024/day12/input.txt").mapIndexed { rowIndex, row -> row.mapIndexed { columnIndex, char -> PointWithChar(Point(rowIndex.toLong(), columnIndex.toLong()), char) } }
+    val seen = mutableSetOf<PointWithChar>()
+    val regions = mutableListOf<Long>()
+
+    input.flatten().forEach { pointWithChar ->
+        if (pointWithChar !in seen) {
+            val entireRegion = getAllPlantsInRegionOfChar(input, pointWithChar)
+            regions.add(entireRegion.first.size * entireRegion.second)
+            seen.addAll(entireRegion.first)
+        }
+    }
+
+    val part1 = regions.sum()
+    println("Part1: $part1")
+
+    val seen2 = mutableSetOf<PointWithChar>()
+    val regions2 = mutableListOf<Long>()
+    input.flatten().forEach { pointWithChar ->
+        if (pointWithChar !in seen2) {
+            val entireRegion = getAllPlantsInRegionOfCharPart2(input, pointWithChar)
+            regions2.add(entireRegion.first.size * entireRegion.second)
+            seen2.addAll(entireRegion.first)
+        }
+    }
+
+    val part2 = regions2.sum()
+    // 823417 too low
+    println("Part2: $part2")
+}
+
+fun getAllPlantsInRegionOfChar(input: List<List<PointWithChar>>, pointWithChar: PointWithChar): Pair<Collection<PointWithChar>, Long> {
+    val seenPointsWithSameChar = mutableSetOf<PointWithChar>()
+    var edges = 0L
+    val remainingPointsToSee = mutableSetOf(pointWithChar)
+    while (remainingPointsToSee.isNotEmpty()) {
+        val currentPoint = remainingPointsToSee.first()
+        val neighbours = input.getNeighbours(currentPoint.point)
+        remainingPointsToSee.addAll(neighbours.filter { it.value == pointWithChar.value && !seenPointsWithSameChar.contains(it)})
+        edges += neighbours.filter { it.value != pointWithChar.value }.size + (4 - neighbours.size)
+        seenPointsWithSameChar.add(currentPoint)
+        remainingPointsToSee.remove(currentPoint)
+    }
+    return seenPointsWithSameChar.filter { it.value == pointWithChar.value } to edges
+}
+
+fun getAllPlantsInRegionOfCharPart2(input: List<List<PointWithChar>>, pointWithChar: PointWithChar): Pair<Collection<PointWithChar>, Long> {
+    val seenPointsWithSameChar = mutableSetOf<PointWithChar>()
+    var corners = 0L
+    val remainingPointsToSee = mutableSetOf(pointWithChar)
+    while (remainingPointsToSee.isNotEmpty()) {
+        val currentPoint = remainingPointsToSee.first()
+        val neighbours = input.getNeighboursWithDirection(currentPoint.point)
+        remainingPointsToSee.addAll(neighbours.mapNotNull { it.first }.filter { it.value == pointWithChar.value && !seenPointsWithSameChar.contains(it)})
+        corners += input.countCorners(currentPoint)
+        seenPointsWithSameChar.add(currentPoint)
+        remainingPointsToSee.remove(currentPoint)
+    }
+    return seenPointsWithSameChar.filter { it.value == pointWithChar.value } to corners
+}
+
